d0dda7cf495e9139e73a5f1b5036b5c21993c2df
almafa64 <bhatalyak@gmail.com>
2023-12-25 03:14:02 +0100

    Add project files.

---
+++ Translator/LanguagePatch.cs
@@ -0,0 +1,90 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+
+namespace Translator
+{
+	internal static class LanguagePatch
+	{
+		//public static string[] en = { "en", "play", "start!", "online", "settings", "exit", "sfxvol", "musicvol", "abilities", "screen shake", "rumble", "resolution", "save", "on", "off", "high", "fullscreen", "windowed", "borderless", "screen", "click to join!", "ready!", "color", "team", "rebind keys", "click jump", "click ability_left", "click ability_right", "click ability_top", "click move_left", "click move_down", "click move_right", "click move_up", "vsync", "nothing", "hide", "names", "names and avatars", "mouse only", "local game", "click to start!", "next level", "ability select", "winner!!", "winners!!", "draw!", "wishlist bopl battle!", "choosing...", "leave game?", "invite friend", "practice", "hold down", "to aim", "to throw grenade", "to dash", "click", "credits", "back", "tutorial", "your lobby is empty", "invite a friend to play online", "not available in demo", "bow", "tesla coil", "engine", "smoke", "invisibility", "platform", "meteor", "random", "missile", "black hole", "rock", "push", "dash", "grenade", "roll", "time stop", "blink gun", "gust", "mine", "revival", "spike", "shrink ray", "growth ray", "chain", "time lock", "throw", "teleport", "grappling hook", "drill" };
+		public static string[] hu = { "hu", "játsz", "kezdés!", "online", "beállítások", "kilépés", "sfxvol", "musicvol", "abilities", "screen shake", "rumble", "resolution", "save", "on", "off", "high", "fullscreen", "windowed", "borderless", "screen", "click to join!", "ready!", "color", "team", "rebind keys", "click jump", "click ability_left", "click ability_right", "click ability_top", "click move_left", "click move_down", "click move_right", "click move_up", "vsync", "nothing", "hide", "names", "names and avatars", "mouse only", "local game", "click to start!", "next level", "ability select", "winner!!", "winners!!", "draw!", "wishlist bopl battle!", "choosing...", "leave game?", "invite friend", "practice", "hold down", "to aim", "to throw grenade", "to dash", "click", "credits", "back", "tutorial", "your lobby is empty", "invite a friend to play online", "not available in demo", "bow", "tesla coil", "engine", "smoke", "invisibility", "platform", "meteor", "random", "missile", "black hole", "rock", "push", "dash", "grenade", "roll", "time stop", "blink gun", "gust", "mine", "revival", "spike", "shrink ray", "growth ray", "chain", "time lock", "throw", "teleport", "grappling hook", "drill" };
+
+		public enum MyLanguage
+		{
+			HU = 14
+		}
+
+		public static Language GetLanguage(this MyLanguage l) => (Language)l;
+
+		static MethodInfo _updateText;
+		static MethodInfo _localTable;
+
+		public static unsafe void Init()
+		{
+			_updateText = typeof(LocalizedText).GetMethod(nameof(LocalizedText.UpdateText));
+			Translator.harmonyInstance.Patch(_updateText, prefix: new HarmonyMethod(Utils.GetMethod(nameof(Patch))));
+			
+			_localTable = typeof(LocalizationTable).GetMethod(nameof(LocalizationTable.GetText));
+			Translator.harmonyInstance.Patch(_localTable, prefix: new HarmonyMethod(Utils.GetMethod(nameof(Patch2))));
+
+			// read languages
+		}
+
+		internal static bool Patch(LocalizedText __instance)
+		{
+			Language currentLanguage = Settings.Get().Language;
+			if ((int)currentLanguage <= Utils.MaxOfEnum<Language>()) return true;
+
+			// own language
+			TMP_FontAsset font = LocalizedText.localizationTable.GetFont(Language.EN, __instance.useFontWithStroke);
+
+			Traverse traverse = Traverse.Create(__instance);
+			traverse.Field("currentLanguage").SetValue(currentLanguage);
+			TextMeshProUGUI textToLocalize = (TextMeshProUGUI)traverse.Field("textToLocalize").GetValue();
+			string enText = (string)traverse.Field("enText").GetValue();
+			TextMesh textToLocalize2 = (TextMesh)traverse.Field("textToLocalize2").GetValue();
+
+			if (textToLocalize == null)
+			{
+				textToLocalize2.text = LocalizedText.localizationTable.GetText(enText, currentLanguage);
+				return false;
+			}
+
+			if (!__instance.useFontWithStroke)
+			{
+				textToLocalize.fontStyle = FontStyles.Normal;
+			}
+			else
+			{
+				textToLocalize.fontStyle = FontStyles.Bold;
+			}
+
+			textToLocalize.text = LocalizedText.localizationTable.GetText(enText, currentLanguage);
+
+			if (!__instance.ignoreFontChange && textToLocalize.font != font)
+			{
+				textToLocalize.font = font;
+			}
+
+			return false;
+		}
+
+		internal static bool Patch2(LocalizationTable __instance, ref string __result, string __0, Language __1)
+		{
+			if ((int)__1 <= Utils.MaxOfEnum<Language>()) return true;
+
+			if (__1 == MyLanguage.HU.GetLanguage())
+			{
+				__result = (string)Traverse.Create(__instance).Method("getText", __0, hu).GetValue();
+			}
+			else __result = __0;
+
+			return false;
+		}
+	}
+}
---
+++ Translator/Translator.cs
@@ -0,0 +1,152 @@
+﻿using Harmony;
+using HarmonyLib;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Runtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Translator
+{
+	public class Translator : MelonMod
+	{
+		public static new HarmonyLib.Harmony harmonyInstance;
+
+		public override void OnInitializeMelon()
+		{
+			harmonyInstance = HarmonyInstance;
+
+			try
+			{
+				LanguagePatch.Init();
+			}
+			catch (Exception e)
+			{
+				MelonLogger.Warning("Error at Language: " + Environment.NewLine + e);
+			}
+		}
+
+		public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+		{
+			if(sceneName == "MainMenu")
+			{
+				GameObject langMenu = GameObject.Find("LanguageMenu_leaveACTIVE");
+				int lastOGLanguage = langMenu.transform.childCount - 1;
+				GameObject langArrowsParent = UnityEngine.Object.Instantiate(GameObject.Find("Resolution"));
+				GameObject lang = UnityEngine.Object.Instantiate(GameObject.Find("en"), langMenu.transform);
+
+				Button[] buttons = langArrowsParent.GetComponentsInChildren<Button>();
+				lang.GetComponentInChildren<SelectionBorder>().ButtonsWithTextColors = buttons;
+				foreach (Button button in buttons)
+				{
+					button.transform.SetParent(lang.transform);
+					button.transform.localScale = new Vector3(1, 1, 1);
+				}
+				buttons[0].transform.localPosition = new Vector3(370, 35, 0);
+				buttons[1].transform.localPosition = new Vector3(-300, 35, 0);
+				UnityEngine.Object.Destroy(langArrowsParent);
+
+				UnityEngine.Object.Destroy(lang.GetComponent<OptionsButton>());
+				LanguageSelector selector = lang.AddComponent<LanguageSelector>();
+				selector.Init();
+				selector.langMenu = langMenu;
+
+				// add readed languages
+				selector.languages.Add("HU");
+				selector.languages.Add("Convex");
+				selector.languages.Add("Nig");
+
+				MainMenu menu = langMenu.GetComponent<MainMenu>();
+				menu.ConfiguredMenuItems.Add(lang);
+				Traverse menuTraverse = Traverse.Create(menu);
+				menuTraverse.Field("Indices").GetValue<List<int>>().Add(0);
+				menuTraverse.Field("MenuItemTransforms").GetValue<List<RectTransform>>().Add(lang.GetComponent<RectTransform>());
+				menuTraverse.Field("MenuItems").GetValue<List<IMenuItem>>().Add(lang.GetComponent<LanguageSelector>());
+
+				List<Vector2> poses = menuTraverse.Field("originalMenuItemPositions").GetValue<List<Vector2>>();
+				float diff = poses[0].y - poses[2].y;
+				poses.Add(new Vector2(0, poses[lastOGLanguage].y - diff));
+
+				lang.name = "Custom Languages";
+				CallOnHover call = lang.GetComponent<CallOnHover>();
+				UnityEvent hover = call.onHover;
+				hover.RemoveAllListeners();
+				hover.AddListener(() => {
+					menu.Select(14);
+					selector.InputActive = true;
+				});
+				call.onExitHover.AddListener(() => {
+					selector.InputActive = false;
+				});
+				UnityEvent click = call.onClick;
+				click.RemoveAllListeners();
+				click.AddListener(() => selector.Click());
+			}
+		}
+	}
+
+	public class LanguageSelector : MonoBehaviour, IMenuItem
+	{
+		public bool InputActive { get; set; }
+		private BindInputAxisToButton[] InputArrows;
+		public int optionIndex { get; private set; } = 0;
+		public TextMeshProUGUI textMesh;
+		public List<string> languages = new List<string>();
+		public GameObject langMenu;
+
+		public void Init()
+		{
+			optionIndex = (int)Settings.Get().Language;
+			int max = Utils.MaxOfEnum<Language>();
+			if (optionIndex > max) optionIndex = optionIndex - max - 1;
+			else optionIndex = 0;
+
+			InputArrows = GetComponentsInChildren<BindInputAxisToButton>();
+			textMesh = GetComponentInChildren<TextMeshProUGUI>();
+			textMesh.font = LocalizedText.localizationTable.enFontAsset;
+
+			Button left = InputArrows[1].GetComponent<Button>();
+			left.onClick.RemoveAllListeners();
+			left.onClick.AddListener(Previous);
+
+			Button right = InputArrows[0].GetComponent<Button>();
+			right.onClick.RemoveAllListeners();
+			right.onClick.AddListener(Next);
+		}
+
+		private void Update()
+		{
+			textMesh.text = languages[optionIndex];
+			for (int i = 0; i < InputArrows.Length; i++)
+			{
+				InputArrows[i].enabled = InputActive;
+			}
+		}
+
+		public void Next()
+		{
+			optionIndex = (optionIndex + 1) % languages.Count;
+			Update();
+		}
+
+		public void Previous()
+		{
+			optionIndex = (optionIndex - 1 + languages.Count) % languages.Count;
+			Update();
+		}
+
+		public void Click()
+		{
+			if (InputActive && isActiveAndEnabled)
+			{
+				langMenu.GetComponent<LanguageMenu>().SetLanguage(Utils.MaxOfEnum<Language>() + 1 + optionIndex);
+				GameObject.Find("mainMenu_leaveACTIVE").GetComponent<MainMenu>().EnableAll();
+				langMenu.GetComponent<MainMenu>().DisableAll();
+				AudioManager.Get().Play("return3");
+			}
+		}
+	}
+}
